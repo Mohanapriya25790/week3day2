e9ee07dd75bfbadc833d7adba065fb81b45b2699
Mohanapriya25790 <103888244+Mohanapriya25790@users.noreply.github.com>
2022-06-01 15:23:03 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,19 @@
+
+public class Automation extends MultipleLanguage {
+
+	public static void main(String[] args) {
+		Automation a = new Automation();
+		
+	}
+
+	public void Selenium() {
+		System.out.println("Selenium");
+		
+	}
+
+	public void Java() {
+		System.out.println("java");
+		
+	}
+a.Java();
+
---
+++ MultipleLanguage.java
@@ -0,0 +1,12 @@
+
+public abstract class MultipleLanguage implements TestTool,Language{
+
+	public void phyton() {
+		System.out.println("Phyton");
+		
+
+	}
+public void ruby() {
+	System.out.println("ruby");
+}
+}
